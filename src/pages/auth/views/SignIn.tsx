25e93f5a29002e98c17189193bcf1140deda3aea
Kelvin Ward <kelvinward1010@gmail.com>
2024-08-25 17:46:15 +0700

    update ui signin

--- src/pages/auth/views/SignIn.tsx
+++ src/pages/auth/views/SignIn.tsx
@@ -1,7 +1,55 @@
+import { Logo } from "@/assets/svg";
+import styles from "./SignIn.module.scss";
+import { Button, Form, Input, notification } from "antd";
+
+type FieldType = {
+    name?: string;
+};
+
 export function SignIn() {
+    const onFinish = (values: FieldType) => {
+        const data = {
+            name: values.name,
+        };
+        console.log(data);
+    };
+
+    const onFinishFailed = (errorInfo: any) => {
+        notification.error({
+            message: `Could not sign in. Please try again!`,
+            description: ` ${errorInfo}`,
+        });
+    };
+
     return (
-        <div>
-            <h4>SignIn</h4>
+        <div className={styles.container}>
+            <div className={styles.head}>
+                <img src={Logo} alt="logo" />
+            </div>
+            <div className={styles.main}>
+                <div className={styles.center}>
+                    <Form
+                        name="signin"
+                        labelCol={{ span: 8 }}
+                        wrapperCol={{ span: 24 }}
+                        style={{ maxWidth: 600 }}
+                        initialValues={{ remember: true }}
+                        onFinish={onFinish}
+                        onFinishFailed={onFinishFailed}
+                        layout={"vertical"}
+                    >
+                        <Form.Item<FieldType> label="Email" name="name">
+                            <Input />
+                        </Form.Item>
+
+                        <Form.Item wrapperCol={{ offset: 7, span: 24 }}>
+                            <Button className={styles.button} htmlType="submit">
+                                Sign In
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </div>
+            </div>
         </div>
     );
 }
--- src/pages/home/components/Header.tsx
+++ src/pages/home/components/Header.tsx
@@ -1,12 +1,17 @@
 import { Logo } from "@/assets/svg";
 import styles from "./Header.module.scss";
 import { ButtonConfig } from "@/components/buttonconfig";
+import { useNavigate } from "react-router-dom";
+import { signinUrl } from "@/routes/urls";
 
 function Header() {
+    const navigate = useNavigate();
+
+    const goSignIn = () => navigate(signinUrl);
     return (
         <div className={styles.container}>
             <img src={Logo} alt="logo" />
-            <ButtonConfig lable={"Sign In"} />
+            <ButtonConfig lable={"Sign In"} onClick={goSignIn} />
         </div>
     );
 }
